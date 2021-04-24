d2a602f179226019cd046b495dabd9820c207acb
Lucas Zylbersztejn <lucaszylbersztejn@Lucass-MacBook-Air.local>
2021-04-25 00:51:23 +0200

    django first

---
+++ mysite/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import include, path
+'''
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
+'''
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
